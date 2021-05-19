2200278a1f179cd615bfb133c3863a6fa0bb4c66
hyungchul pak <jasotn12@naver.com>
2021-05-19 20:10:32 +0900

    Chapter 05 스프링 시큐리티와 OAuth 2.0으로  로그인 구현하기 (2)

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -46,9 +46,8 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-
         return OAuthAttributes.builder()
-                .nameAttributeKey((String) response.get("name"))
+                .name((String) response.get("name"))
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
